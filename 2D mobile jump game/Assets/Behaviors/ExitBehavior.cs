28a2c5f6e81628d2d2385f0ff281a761d2b087d1
kirbinator505 <10841219@my.uvu.edu>
2021-12-08 22:51:47 -0700

    fixed ui issues and added an exit button

---
+++ 2D mobile jump game/Assets/Behaviors/ExitBehavior.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ExitBehavior : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
